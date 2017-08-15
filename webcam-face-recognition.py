c679074b8239880ca7711072fea66b30e9d471bb
wwwins <jacky.huang@isobar.com>
2017-08-15 12:25:58 +0800

    Added dynamic training

--- webcam-face-recognition.py
+++ webcam-face-recognition.py
@@ -3,7 +3,7 @@
 # @Author: wwwins
 # @Date:   2017-08-09 11:15:17
 # @Last Modified by:   wwwins
-# @Last Modified time: 2017-08-14 16:44:18
+# @Last Modified time: 2017-08-15 12:23:24
 
 import cv2
 import sys
@@ -13,7 +13,7 @@ from ticket import ticket
 from ImageText import *
 from crop_face import *
 
-DEBUG = 1
+DEBUG = 0
 ENABLE_FPS = False
 
 FRAME_WIDTH = 640
@@ -27,7 +27,7 @@ SIGHT_COLOR = (66,66,244)
 # 依不同的 cascade 做調整
 # lbpcascade_frontalface: 1.1
 # haarcascade_frontalface_alt2: 1.3
-SCALE_FACTOR = 1.1
+SCALE_FACTOR = 1.3
 MIN_NEIGHBORS = 3
 #MIN_SIZE = 30
 MIN_SIZE = 80
@@ -35,7 +35,7 @@ MIN_SIZE = 80
 FACE_SIZE = 200
 
 # default tolerance value
-TOLERANCE = 0.6
+TOLERANCE = 0.7
 
 if len(sys.argv) < 3:
     print("""
@@ -55,8 +55,14 @@ recognizer = cv2.face.createLBPHFaceRecognizer()
 # recognizer = cv2.face.createEigenFaceRecognizer()
 recognizer.load(model_file)
 
+arr_images = []
+arr_labels = []
+label_id = 3
+
 millis = lambda: int(round(time.time() * 1000))
 started_waiting_at = millis()
+cnt_error = 0
+error_max = 30
 
 image = None
 if len(sys.argv)>3:
@@ -104,15 +110,59 @@ def faceDetect(frame):
         minSize=(MIN_SIZE, MIN_SIZE),
         flags=cv2.CASCADE_SCALE_IMAGE
     )
-    eyes = None
-    roi_frame = np.array([])
     if len(faces)>0:
-        x, y, w, h = faces[0]
-        roi_frame = frame[y:y+h,x:x+w]
-        eyes = eyeCascade.detectMultiScale(roi_frame)
-    return faces, eyes, roi_frame
+        for (x,y,w,h) in faces:
+            cv2.rectangle(frame, (x,y), (x+w,y+h), (0,0,255), 1)
+    return frame
 
-def facePrediction(faces, eyes, roi_gray):
+def faceDetectAndCrop(frame):
+    faces = faceCascade.detectMultiScale(
+        frame,
+        scaleFactor=SCALE_FACTOR,
+        minNeighbors=MIN_NEIGHBORS,
+        minSize=(MIN_SIZE, MIN_SIZE),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+    if len(faces)<1:
+        return frame,[]
+    if len(faces)>0:
+        for (x,y,w,h) in faces:
+            cv2.rectangle(frame, (x,y), (x+w,y+h), (0,0,255), 1)
+        return frame,faces
+
+def faceTrain(frame):
+    global cnt_error,arr_images,arr_labels,label_id
+    if cnt_error>error_max:
+        print("need training:"+str(label_id+1))
+        label_id = label_id + 1
+        arr_images.append(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
+        arr_labels.append(label_id)
+        recognizer.update(arr_images, np.array(arr_labels))
+        cnt_error = 0
+
+def facePrediction(frame):
+    global cnt_error
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    collector = cv2.face.MinDistancePredictCollector()
+    prediction_label = recognizer.predict(gray)
+    recognizer.predict(gray,collector)
+    prediction_label = collector.getLabel()
+    prediction_distance = collector.getDist()/100
+    if (DEBUG):
+        print("label->dist: {0}->{1}".format(prediction_label, prediction_distance))
+    if (prediction_distance < TOLERANCE):
+        # draw_hunting_sight(frame, (x,y), (x+w,y+h))
+        showText = "Unknown"
+        if(prediction_label>=0):
+            showText = recognizer.getLabelInfo(prediction_label)
+            # cv2.putText(frame, str(showText), (x,y-15), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 1)
+            # cv2.putText(frame, str(prediction_label), (x,y-15), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 1)
+            prediction_text(prediction_label, "號大頭照:{:.2f}".format(prediction_distance))
+    else:
+        cnt_error = cnt_error+1
+        prediction_text(999999,"Unknown")
+
+def facePredictionWithEyes(faces, eyes, roi_gray):
     gray_resize = np.array([])
     collector = cv2.face.MinDistancePredictCollector()
     x, y, w, h = faces[0]
@@ -155,6 +205,7 @@ def facePrediction(faces, eyes, roi_gray):
     return gray_resize
 
 def show_image_text(contents):
+    global cnt_error
     print('show image text:'+contents)
     frame_title = get_image_text(contents)
     cv2.imshow("Label name",frame_title)
@@ -162,14 +213,16 @@ def show_image_text(contents):
 def prediction_text(label, text):
     global started_waiting_at
     if (millis() - started_waiting_at) > 500:
-        show_image_text(str(label)+':'+text)
+        show_image_text(str(label)+' '+text)
         started_waiting_at = millis()
 
 def main():
     cv2.namedWindow('Video')
-    cv2.moveWindow("Video", 690+640, 750+150)
+    # cv2.moveWindow("Video", 690+640, 750+150)
     cv2.namedWindow("Label name")
-    cv2.moveWindow("Label name", 690+640, 750+150+FRAME_HEIGHT+24)
+    # cv2.moveWindow("Label name", 690+640, 750+150+FRAME_HEIGHT+24)
+    cv2.namedWindow('Crop')
+    cv2.moveWindow("Crop", 700, 100)
 
     if image:
         frame = cv2.imread(image)
@@ -178,7 +231,7 @@ def main():
             if DEBUG:
                 print ("*** not found faces ***")
             prediction_text(999999,"Unknown")
-        result = facePrediction(faces, eyes, roi_gray);
+        result = facePredictionWithEyes(faces, eyes, roi_gray);
         cv2.imshow('Video', frame)
         if(len(result)>0):
             cv2.imshow('Result', result)
@@ -189,17 +242,13 @@ def main():
         # Capture frame-by-frame
         _,frame = video_capture.read()
         frame = cv2.flip(frame,1)
+        frame, faces = faceDetectAndCrop(frame)
         cv2.imshow('Video', frame)
-
-        faces, eyes, roi_gray = faceDetect(frame)
-        if len(faces)<1:
-            prediction_text(999999,"Unknown")
-            continue
-        result = facePrediction(faces, eyes, roi_gray);
-        # Display the resulting frame
-        if DEBUG:
-            if(len(result)>0):
-                cv2.imshow('Result', result)
+        if len(faces)>0:
+            x,y,w,h = faces[0]
+            cv2.imshow('Crop', frame[y+1:y+h, x+1:x+w])
+            faceTrain(frame[y:y+h, x:x+w])
+            facePrediction(frame[y:y+h, x:x+w])
         if ENABLE_FPS:
             print("fps:",t.fps())
         if cv2.waitKey(1) & 0xFF == ord('q'):
