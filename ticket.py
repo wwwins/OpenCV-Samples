a381d50c20a31ab9ce96a7ab0790f014a0beb5c0
wwwins <jacky.huang@isobar.com>
2019-05-09 14:53:25 +0800

    Added calculating opencv fps

--- ticket.py
+++ ticket.py
@@ -1,25 +1,32 @@
+# -*- coding: utf-8 -*-
+#
+# ticket.py
+#
+# Usage:
+#       from ticket import ticket
+#       t = ticket()
+#       t.display()
+#
+import sys
 import cv2
-
-'''
-	from ticket import ticket
-	t = ticket()
-	t.fps()
-'''
 
 class ticket(object):
 
     def __init__(self):
-        self._count     = 0
-        self._oldCount  = 0
-        self._freq      = 1000 / cv2.getTickFrequency()
-        self._startTime = cv2.getTickCount()
+        self._step = 3
+        self._frameCount = 0
+        self._freq = cv2.getTickFrequency()
+        self._prevFrameTime = cv2.getTickCount()
 
     def fps(self):
-        nowTime         = cv2.getTickCount()
-        diffTime        = (nowTime - self._startTime) * self._freq
-        self._startTime = nowTime
-        fps             = (self._count - self._oldCount) / (diffTime / 1000.0)
-        self._oldCount  = self._count
-        self._count     += 1
+        self._frameCount += 1
+        nowFrameTime = cv2.getTickCount()
+        fps = self._freq / (nowFrameTime - self._prevFrameTime)
+        self._prevFrameTime = nowFrameTime
         fpsRounded      = round(fps, 1)
         return fpsRounded
+
+    def display(self):
+        sys.stdout.write("\rfps:{}".format(self.fps()))
+        if (self._frameCount%self._step==0):
+            sys.stdout.flush()
